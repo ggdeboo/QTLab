e0703451d60ab770c3d2f2e14587f5c9592e4c2e
Gabriele de Boo <ggdeboo@gmail.com>
2015-05-11 15:45:50 +1000

    Update to the Delta PSC232 wrapper
    More improvements
    I haven't figured out yet how to reset the instrument, it doesn't accept
    '*RST'. This prevents the wrapper from properly reloading the instrument
    in QTlab.

--- bilby/instrument_plugins/Delta_PSC232.py
+++ bilby/instrument_plugins/Delta_PSC232.py
@@ -19,74 +19,101 @@ from instrument import Instrument
 import types
 import visa
 import logging
+from time import sleep
 
 class Delta_PSC232(Instrument):
 
-    def __init__(self, name, address=None, channel_list=(1, 2), reset=False):
+    def __init__(self, 
+                name, 
+                address=None, 
+                channel_list=[1, 2], 
+                minvoltage_list=[0,0],
+                maxvoltage_list=[30,30],
+                mincurrent_list=[0,0],
+                maxcurrent_list=[5,5],
+                reset=False):
         Instrument.__init__(self, name, tags=['measure'])
 
         self._address = address
 #	self._term_chars = '\n\r\x04'
         self._channel_list = channel_list
         self._visains = visa.instrument(address) #, term_chars = "\n\r\x04") # Hoo Rah
-        self._visains.baud_rate = 4800L
+        self._visains.baud_rate = 4800
+        self._visains.timeout = 0.2
         self._visains.clear()
         # identification
-#        self._idn = []
-#        for idx, ch in enumerate(channel_list):
-#            self._idn.append('')
-#            self._visains.write('CH %i' % ch)
-#            self._idn[idx] += self._visains.ask('*IDN?')
-#            self._idn[idx] += self._visains.read()
-#            self._idn[idx] += self._visains.read()
-#        print self._idn
+        self._idn = []
 
-        if reset:
-            self._visains.write("*R")               # Reset the instrument
-
-#        self._visains.write("CH "+str(channel)) # Talking to the correct instrument
-#        for psc_channel in channel_list:
+        for idx, ch in enumerate(channel_list):
+            self._idn.append('')
+            self._visains.write('CH %i' % ch)
+            self._idn[idx] += self._check_response(self._visains.ask('*IDN?'))
+            self._idn[idx] += self._check_response(self._visains.read())
+            self._idn[idx] += self._check_response(self._visains.read())
+        print self._idn
+        
         self.add_parameter('minimum_voltage', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
-                    channels=channel_list)
+                    channels=channel_list,
+                    units='V',
+                    channel_prefix='ch%s_')
         self.add_parameter('maximum_voltage', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
-                    channels=channel_list)
+                    channels=channel_list,
+                    units='V',
+                    channel_prefix='ch%s_')
         self.add_parameter('minimum_current', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
-                    channels=channel_list)
+                    channels=channel_list,
+                    units='A',
+                    channel_prefix='ch%s_')
         self.add_parameter('maximum_current', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
-                    channels=channel_list)
+                    channels=channel_list,
+                    units='A',
+                    channel_prefix='ch%s_')
         self.add_parameter('voltage', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
                     tags=['sweep'],
                     maxstep=0.1, stepdelay=100,
                     channels=channel_list,
-                    units='V')
+                    units='V',
+                    channel_prefix='ch%s_')
         self.add_parameter('voltage_measured', type=types.FloatType,
                     flags=Instrument.FLAG_GET,
                     channels=channel_list,
-                    units='V')
+                    units='V',
+                    channel_prefix='ch%s_')
         self.add_parameter('current', type=types.FloatType,
                     flags=Instrument.FLAG_SET | Instrument.FLAG_SOFTGET,
                     tags=['sweep'],
                     maxstep=0.1, stepdelay=100,
                     channels=channel_list,
-                    units='A')
+                    units='A',
+                    channel_prefix='ch%s_')
         self.add_parameter('current_measured', type=types.FloatType,
                     flags=Instrument.FLAG_GET,
                     channels=channel_list,
-                    units='A')
-
+                    units='A',
+                    channel_prefix='ch%s_')
         self.add_parameter('active_channel', type=types.IntType,
                         flags=Instrument.FLAG_GET)
 
+        for idx, ch in enumerate(channel_list):
+            self.set_parameter_options('ch%i_voltage' % ch,
+                                        minval=minvoltage_list[idx],
+                                        maxval=maxvoltage_list[idx])
+            self.set_parameter_options('ch%i_current' % ch,
+                                        minval=mincurrent_list[idx],
+                                        maxval=maxcurrent_list[idx])
 #        self.add_function('set_defaults')
         self.add_function('get_all')
+        self.add_function('reset_channel')
    
+#        self._channel_has_been_reset = len(channel_list)*[False]
         if reset:
             self.set_defaults()
+
         self.get_all()
 
     def set_defaults(self):
@@ -94,17 +121,31 @@ class Delta_PSC232(Instrument):
         
         These are accurate for the ES 030-5 power supplies
         '''
-        self.set_minimum_voltage1(0)
-        self.set_maximum_voltage1(30)
-        self.set_minimum_current1(0)
-        self.set_maximum_current1(5)
-        self._visains.write("REMOTE")
+        for channel in self._channel_list:
+            self.set('ch%i_minimum_voltage' % channel, 0)
+            self.set('ch%i_maximum_voltage' % channel, 30)
+            self.set('ch%i_minimum_current' % channel, 0)
+            self.set('ch%i_maximum_current' % channel, 5)
+            self.set_parameter_options('ch%i_voltage' % channel, value=0.0)
+            self.set_parameter_options('ch%i_current' % channel, value=0.0)
+        # it doesn't like getting these commands twice...
+            self._visains.write("REMOTE")
 
     def get_all(self):
 #        for ch in self.get_
 #        self.get_voltage()
 #        self.get_current()
         self.get_active_channel()
+        for channel in self._channel_list:
+            self.get('ch%i_current_measured' % channel)
+            self.get('ch%i_voltage_measured' % channel)
+
+    def reset_channel(self, channel):
+        '''Not sure whether this works properly
+        Instrument doesn't seem to recognize *RST
+        '''
+        self._visains.write('CH %i' % channel)
+        self._visains.write('*RST')
 
     def do_set_minimum_voltage(self, minvol, channel):
         '''Set minimum of the voltage range
@@ -170,9 +211,12 @@ class Delta_PSC232(Instrument):
         response = self._visains.ask("CH?")
         return int(response.lstrip('\x04'))
 
-#    class controlled_communication(self):
-#        self._visains.write('CH %i' % self._channel)
-#        def __exit__(self):
-#            pass
-    
+    def _check_response(self, response):
+        '''Check the response for error messages'''
+        response = response.lstrip('\x04')
+        if response.lstrip(' ').startswith('unknown command !'):
+            logging.warning('Instrument replied with unknown command')
+            new_response = self._check_response(self._visains.read())
+            return new_response
+        return response
 
