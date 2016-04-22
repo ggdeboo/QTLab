de77a6b023912807b8a4a25ca95da9c578da17c5
Gabriele de Boo <ggdeboo@gmail.com>
2016-04-22 11:28:03 +1000

    Rohde & Schwarz FSL6 spectrum analyzer update
    - more pythonic parameter names
    - detection of several installed options
    - added bandwidth_resolution, input_attenuation, preamp_status and
    input_impedance

--- bilby/instrument_plugins/RS_FSL6.py
+++ bilby/instrument_plugins/RS_FSL6.py
@@ -1,5 +1,6 @@
 # RS_FSL6.py class, to perform the communication between the Wrapper and the device
 # Pablo Asshoff <techie@gmx.de>, 2013
+# Gabriele de Boo <ggdeboo@gmail.com>, 2016
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -51,8 +52,28 @@ class RS_FSL6(Instrument):
         self._visainstrument = visa.instrument(self._address)
         self._visainstrument.timeout = 1
 
+        self._installed_options = self._visainstrument.ask('*OPT?')
+        bw_min = 300
+
+        if 'B4' in self._installed_options:
+            logging.info('Option B4 (OCXO reference frequency) is installed')
+            logging.info(
+                'Aging per year and 50 degrees temperature drift: .1 ppm')
+        else:
+            logging.info('Standard reference frequency is installed ')
+            logging.info(
+                'Aging per year and 50 degrees temperature drift: 1 ppm')
+        if 'B7' in self._installed_options:
+            logging.info('Option B7 (narrow resolution filters) is installed')
+            bw_min = 10
+        if 'B10' in self._installed_options:
+            logging.info('Option B10 (GPIB interface) is instaleld')
+        if 'B22' in self._installed_options:
+            logging.info('Option B22 (RF amplifier) is installed')
+
         # Add parameters
-        self.add_parameter('centerfrequency', type=types.FloatType,
+        # frequency section
+        self.add_parameter('center_frequency', type=types.FloatType,
             flags=Instrument.FLAG_GETSET | Instrument.FLAG_GET_AFTER_SET,
             minval=9e3, maxval=6e9,
             units='Hz', format='%.04e')
@@ -60,15 +81,33 @@ class RS_FSL6(Instrument):
             flags=Instrument.FLAG_GETSET | Instrument.FLAG_GET_AFTER_SET,
             minval=0, maxval=3e9, 
             units='Hz', format='%.04e')
-        self.add_parameter('referencelevel', type=types.FloatType,
+        self.add_parameter('bandwidth_resolution', type=types.FloatType,
+            flags=Instrument.FLAG_GET,
+            units='Hz', format='%.0f',
+            minval=bw_min, maxval=10e6)
+        
+        # input section
+        self.add_parameter('reference_level', type=types.FloatType,
             flags=Instrument.FLAG_GETSET | Instrument.FLAG_GET_AFTER_SET,
             minval=-80, maxval=30,
             units='dBm', format='%.04e')
-        self.add_parameter('sweeptime', type=types.FloatType,
+        self.add_parameter('input_attenuation', type=types.FloatType,
+            flags=Instrument.FLAG_GET,
+            minval=-30, maxval=0, minstep=5.0,
+            units='dB', format='%.1f')
+        self.add_parameter('input_impedance', type=types.IntType,
+            flags=Instrument.FLAG_GET,
+            option_list=(50, 75), units='Ohm')
+        if 'B22' in self._installed_options:
+            self.add_parameter('preamp_status', type=types.BooleanType,
+                flags=Instrument.FLAG_GET,
+                )
+
+        self.add_parameter('sweep_time', type=types.FloatType,
             flags=Instrument.FLAG_GETSET | Instrument.FLAG_GET_AFTER_SET,
             minval=1e-6, maxval=60,
             units='s', format='%.04e')
-        self.add_parameter('channelpower', type=types.FloatType,
+        self.add_parameter('channel_power', type=types.FloatType,
             flags=Instrument.FLAG_GET, 
             units='W', format='%.10e')
         self.add_parameter('timetracemarkerpower', type=types.FloatType,
@@ -99,7 +138,9 @@ class RS_FSL6(Instrument):
         self.add_function('init_trace_readout')
         self.add_function('start_sweep')
         self.add_function('stop_power_measurement')
-        self.add_function('convert_dBuV_to_V')  # works but apparently not needed, device returns usually V by default
+        self.add_function('convert_dBuV_to_V')  # works but apparently not 
+                                                # needed, device returns 
+                                                # usually V by default
 #        self.add_function('stop_remote_control')
 #        self.add_function('trigger_mode')
 #        self.add_function('enable_trigger')
@@ -137,11 +178,16 @@ class RS_FSL6(Instrument):
             None
         '''
         logging.info(__name__ + ' : reading all settings from instrument')
-        self.get_centerfrequency()
+        self.get_center_frequency()
         self.get_span()
-        self.get_referencelevel()
-        self.get_sweeptime()
-        self.get_channelpower()
+        self.get_bandwidth_resolution()
+        self.get_reference_level()
+        self.get_sweep_time()
+        self.get_channel_power()
+        self.get_input_attenuation()
+        self.get_input_impedance()
+        if 'B22' in self._installed_options:
+            self.get_preamp_status()
 
     def do_set_display_onoff(self, type):
         '''
@@ -162,12 +208,6 @@ class RS_FSL6(Instrument):
         else:
             logging.error('invalid type %s' % type)
 
-
-
-
-
-
-
     def init_IQ_measurement(self):
         '''
         Initializes an I/Q measurement
@@ -180,7 +220,8 @@ class RS_FSL6(Instrument):
        '''
         logging.debug(__name__ + ' : initialization of I/Q (quadrature) measurement')
         
-        #setting the trigger level to 0.5 V (bounds: 0.5 V to 3.5 V, p.1590 of operating manual, RST value 1.4 V)
+        #setting the trigger level to 0.5 V (bounds: 0.5 V to 3.5 V, p.1590 
+        # of operating manual, RST value 1.4 V)
         self._visainstrument.write('TRIG:LEV 0.5 V')
         #self._visainstrument.write('BAND:RES 20MHz')     #resultion bandwidth. 20 MHz, only for test
         self._visainstrument.write('FREQ:SPAN 0Hz')        
@@ -207,7 +248,7 @@ class RS_FSL6(Instrument):
         Output:
             None
 
-        maybe add sweeptime parameter etc. later and measure both sidebands for better statistics
+        maybe add sweep time parameter etc. later and measure both sidebands for better statistics
         '''
         logging.debug(__name__ + ' : initialization of channel power measurement')
         self._visainstrument.write('POW:ACH:ACP 0')      
@@ -258,7 +299,6 @@ class RS_FSL6(Instrument):
         self._visainstrument.write('INIT')
         #self._visainstrument.write('*WAI')
 
-
     def init_trace_readout(self):
         '''
         Read a trace
@@ -283,7 +323,6 @@ class RS_FSL6(Instrument):
         #    #the previous command just creates a file locally on the analyer
         ##self._visainstrument.write('TRAC? TRACE1')
       
-
     def convert_dBuV_to_V(self,dBuV):
         '''
         converts dBuV to Volt
@@ -298,7 +337,6 @@ class RS_FSL6(Instrument):
         UinV = 10**(dBuV/20)/1E6    # formula from http://www.kathrein.de/include/pegelumrechnung.cfm
         return UinV
 
-
     def convert_dBm_to_W(self,dBm):
         '''
         converts dBm to Watt
@@ -312,8 +350,6 @@ class RS_FSL6(Instrument):
         logging.debug(__name__ + ' : convert dBm to Watt') 
         PinW = 10**(dBm/10)/1E3    # formula from http://www.kathrein.de/include/pegelumrechnung.cfm
         return PinW
-
-
 
 #    def stop_remote_control(self):
 #        '''
@@ -331,7 +367,7 @@ class RS_FSL6(Instrument):
 
 
     # communication with machine
-    def do_get_centerfrequency(self):
+    def do_get_center_frequency(self):
         '''
         Get center frequency from device
 
@@ -339,29 +375,30 @@ class RS_FSL6(Instrument):
             None
 
         Output:
-            centerfrequency (float) : center frequency in Hz
+            center_frequency (float) : center frequency in Hz
         '''
         logging.debug(__name__ + ' : reading center frequency from instrument')
         return float(self._visainstrument.ask('FREQ:CENT?'))
 
-
-
-
-    def do_set_centerfrequency(self, centerfrequency):
+    def do_set_center_frequency(self, center_frequency):
         '''
         Set center frequency of device
 
         Input:
-            centerfrequency (float) : center frequency in Hz
+            center_frequency (float) : center frequency in Hz
 
         Output:
             None
         '''
-        logging.debug(__name__ + ' : setting center frequency to %s Hz' % centerfrequency)
-        self._visainstrument.write('FREQ:CENT %e' % centerfrequency)
+        logging.debug(__name__ + 
+            ' : setting center frequency to {0} Hz'.format(center_frequency))
+        self._visainstrument.write('FREQ:CENT {0:e}'.format(center_frequency))
 
-
-
+    def do_get_bandwidth_resolution(self):
+        '''
+        Get the resolution bandwidth
+        '''
+        return float(self._visainstrument.ask('SENS:BWID:RES?'))
 
     def do_set_triggerlevel(self, triggerlevel):
         '''
@@ -375,7 +412,6 @@ class RS_FSL6(Instrument):
         '''
         logging.debug(__name__ + ' : setting trigger level to %s V' % triggerlevel)
         self._visainstrument.write('TRIG:LEV %e V' % triggerlevel)        
-
 
     def do_get_span(self):
         '''
@@ -403,7 +439,7 @@ class RS_FSL6(Instrument):
         logging.debug(__name__ + ' : setting span to %s Hz' % span)
         self._visainstrument.write('FREQ:SPAN %e' % span)
 
-    def do_get_referencelevel(self):
+    def do_get_reference_level(self):
         '''
         Get reference level from device
 
@@ -411,49 +447,85 @@ class RS_FSL6(Instrument):
             None
 
         Output:
-            referencelevel (float) : reference level in dBm
+            reference_level (float) : reference level in dBm
         '''
-        logging.debug(__name__ + ' : reading referencelevel from instrument')
+        logging.debug(__name__ + ' : reading reference level from instrument')
         return float(self._visainstrument.ask('DISP:TRAC:Y:RLEV?'))
 
-    def do_set_referencelevel(self,referencelevel):
+    def do_set_reference_level(self, reference_level):
         '''
-        Set referencelevel of device
+        Set reference level of device
 
         Input:
-            referencelevel (float) : reference level in dBm(??)
+            reference_level (float) : reference level in dBm(??)
 
         Output:
             None
         '''
-        logging.debug(__name__ + ' : setting referencelevel to %s dBm' % referencelevel)
-        self._visainstrument.write('DISP:TRAC:Y:RLEV %e' % referencelevel)
+        logging.debug(__name__ + 
+            ' : setting reference level to {0} dBm'.format(reference_level))
+        self._visainstrument.write(
+                        'DISP:TRAC:Y:RLEV {0:e}'.format(reference_level))
 
-    def do_get_sweeptime(self):
+    def do_get_input_attenuation(self):
         '''
-        Get sweeptime level from device
+        Get the input attenuation level
+        '''
+        return float(self._visainstrument.ask('INP:ATT?'))
+
+    def do_get_input_impedance(self):
+        '''
+        Get the input impedance setting. The setting of 75 Ohm should only
+        be used when a 25 Ohm resistor is placed in series with the input.
+        '''
+        return int(self._visainstrument.ask('INP:IMP?'))
+
+    def do_get_preamp_status(self):
+        '''
+        Get the status of the preamp. The preamp adds 20 dB of amplification.
+        
+        Input:
+            None
+        
+        Output:
+            True:   preamp is on
+            False:  preamp is off
+        '''
+        status = self._visainstrument.ask('INP:GAIN:STAT?')
+        if status == '1':
+            return True
+        elif status == '0':
+            return False
+        else:
+            logging.error(
+                'Incorrect response to INP:GAIN:STAT? : {0}'.format(status))
+
+    def do_get_sweep_time(self):
+        '''
+        Get sweep time level from device
 
         Input:
             None
 
         Output:
-            sweeptime (float) : sweep time in s
+            sweep_time (float) : sweep time in s
         '''
-        logging.debug(__name__ + ' : reading sweeptime from instrument')
+        logging.debug(__name__ + ' : reading sweep time from instrument')
         return float(self._visainstrument.ask('SWE:TIME?'))
 
-    def do_set_sweeptime(self,sweeptime):
+    def do_set_sweep_time(self, sweep_time):
         '''
-        Set sweeptime of device
+        Set sweep time of device
 
         Input:
-            sweeptime (float) : sweep time in s
+            sweep_time (float) : sweep time in s
 
         Output:
             None
         '''
-        logging.debug(__name__ + ' : setting sweeptime to %s s' % sweeptime)
-        self._visainstrument.write('SWE:TIME %e' % sweeptime)
+        logging.debug(__name__ + 
+                    ' : setting sweep time to {0} s'.format(sweep_time))
+        self._visainstrument.write('SWE:TIME {0:e}'.format(sweep_time))
 
     def do_get_IQresult(self):
         '''
@@ -473,8 +545,6 @@ class RS_FSL6(Instrument):
         #self._visainstrument.write('INIT,*WAI')        
         #return self._visainstrument.ask('TRAC:IQ:DATA:MEM? 0,4096')
 
-
-
     def do_get_tracedata(self):
         '''
         read out trace data from device
@@ -490,8 +560,7 @@ class RS_FSL6(Instrument):
         #self._visainstrument.write('FORM ASC')      # ASCII file format, alternative: FORMat REAL,32 (binary file)
         return self._visainstrument.ask('TRAC? TRACE1')
 
-
-    def do_get_channelpower(self):
+    def do_get_channel_power(self):
         '''
         Get channel power from device (in MEAS. menu on front panel of the RS-FSL6, 
         command CP/ACP/MC-ACP)
@@ -504,13 +573,11 @@ class RS_FSL6(Instrument):
             None
 
         Output:
-            channelpower (float) : channel power in ??unit
+            channel_power (float) : channel power in ??unit
         '''
-        logging.debug(__name__ + ' : reading channelpower from instrument')
+        logging.debug(__name__ + ' : reading channel_power from instrument')
         self._visainstrument.write('*WAI')    		# Starts a sweep and waits for the end of the sweep.
         return float(self._visainstrument.ask('CALC:MARK:FUNC:POW:RES? ACP'))
-
-
 
     def do_get_timetracemarkerpower(self):
         '''
